5dc0256cfaa24bde23a8fb20f85abf71a37d4dc8
orcnog <orcnog@gmail.com>
2020-09-01 01:54:48 -0500

    fixes hook to init, removes filtering available targets by 'active:true', adds CSS to positon the hidden ghost textarea

--- scripts/autocomplete-whisper.js
+++ scripts/autocomplete-whisper.js
@@ -1,83 +1,89 @@
-Hooks.on("ready", () => {
-
-  // This code runs once core initialization is ready and game data is available.
-
-  // foundry.js's whisperPattern = new RegExp(/^(@|\/w(?:hisper)?\s{1})(\[(?:[^\]]+)\]|(?:[^\s]+))\s+([^]*)/, 'i');
-  const whisperPattern = new RegExp(/^(\s*@\s*\[?\s*|\s*\/w(?:hisper)?\s+\[?\s*)((?<=\[)[^\]]*,\s*)?/, "i"); 
-  const PLAYERS = 'Players';
-  const GM = 'GM';
-
-  (function () {
-
+Hooks.on('renderSidebarTab', (app, html, data) => {
+    if (app.tabName !== "chat") {
+      return;
+    }
+  
+    // This is the all-important whisper syntax matcher.
+    // It must match any of the following examples:
+    //
+    // '@' or '/w ' or '/whisper ' or '/wHiSpEr ' (case-insensitive)
+    // '   @  ' or ' /w     ' etc. (the above syntax proceeded and/or followed by one or more spaces)
+    //
+    // Note: i'm using '@' to represent any of the above syntaxes (like '/w') from here on in this comment.
+    // '@[' (match the start of an Arrays of targets)
+    //
+    // '@[someName,' (an array with any name(s) followed by a comma)
+    //
+    // BUT NOT!...
+    // '@someName ' or '@someName,' or '@someName, ' (stop matching after a non-array whisper target is complete)
+    // '@[someName, otherName] hello,' (stop matching if whisper target is complete)
+    //
+    // For reference, here's foundry.js's whisperPattern, which does not actually meet all our needs...
+    // new RegExp(/^(@|\/w(?:hisper)?\s{1})(\[(?:[^\]]+)\]|(?:[^\s]+))\s+([^]*)/, 'i');
+    //
+    // Note: the Regex pattern below uses a Positive Lookbehind, (?<=\[), which may not be supported in obscure or old browsers.
+    const whisperPattern = new RegExp(/^(\s*@\s*\[?\s*|\s*\/w(?:hisper)?\s+\[?\s*)((?<=\[)[^\]]*,\s*)?/, "i");
+    // when the above regex pattern is used in a split(), define the parts' by array index...
+    const whisperSyntaxIndex = 1;
+    const targetsInArrayIndex = 2;
+  
+    // match if the input (after the initial whispeer syntax) is a list of targets
+    const listOfNamesRegex = new RegExp(/^(\s*@\s*\[\s*|\s*\/w(?:hisper)?\s+\[\s*)[^\]]*,\s*/, "i");
+  
+    // some string constants
+    const PLAYERS = "Players";
+    const GM = "GM";
+  
+    /* Set up markup for our UI to be injected */
     const $whisperMenuContainer = $('<div id="whisper-menu"></div>');
-    const $ghostTextarea = $(
-      '<textarea class="chatghosttextarea" autocomplete="nope" disabled></textarea>'
-    );
-    let $whisperMenu = $(
-      '<nav id="context-menu" class="expand-up"><ol class="context-items"></ol></nav>'
-    );
+    const $ghostTextarea = $('<textarea class="chatghosttextarea" autocomplete="off" readonly disabled></textarea>');
+    let $whisperMenu = $('<nav id="context-menu" class="expand-up"><ol class="context-items"></ol></nav>');
+  
+    /* Add our UI to the DOM */
     $("#chat-message").after($ghostTextarea);
     $("#chat-message").after($whisperMenuContainer);
-
     /* Listen for chat input. Do stuff.*/
-    $("#chat-message").on("input.whisperer", (e) => {
-      handleChatInput();
-    });
-
+    $("#chat-message").on("input.whisperer", handleChatInput);
     /* Listen for "]" to close an array of targets (names) */
-    $("#chat-message").on('keydown.closearray', e => {
-      if (e.which == 221) { // `]`
-        const arrayOfNamesRegex = new RegExp(/^.*\[.*,\s/, "i"); 
-        let val = $("#chat-message").val();
-        if (val.match(arrayOfNamesRegex)) {
-          e.preventDefault();
-          const newval = val.substring(0, val.length - 2); // remove `, ` from the end
-          $("#chat-message").val(newval + "] ");
-          closeWhisperMenu();
-        }
-      }
-    });
-    
-    $("#whisper-menu").on("click", "li", function (e) {
-      e.stopPropagation();
-      var autocompleteText = autocomplete($(this).text());
-      $("#chat-message").val(autocompleteText);
-      $('#chat-message').focus();
-      closeWhisperMenu();
-      if ($("#chat-message").val().indexOf('[') > -1) {
-        handleChatInput();
-      }
-    });
-        
+    $("#chat-message").on("keydown.closearray", listFinishHandler);
+    /* Listen for click on a menu item */
+    $("#whisper-menu").on("click", "li", menuItemClickHandler);
+  
     function handleChatInput() {
       resetGhostText();
       const val = $("#chat-message").val();
       if (val.match(whisperPattern)) {
-        console.log('match');
         let splt = val.split(whisperPattern);
-        let str = splt[splt.length - 1];
-        const activePlayers = game.users.entities.filter(
-          (p) => p.active && p.name
-        );
+        // console.log(splt);
+        let input = splt[splt.length - 1]; // newly typed input
+        let alreadyTargeted = getAlreadyTargeted(splt[targetsInArrayIndex]);
+        const activePlayers = game.users.entities.filter(p => p.role !== 4 && p.name);
         let whisperablePlayers = activePlayers.map((p) => p.name);
         whisperablePlayers.push(PLAYERS);
         whisperablePlayers.push(GM);
-        let matchingPlayers = whisperablePlayers.filter(
-          (p) => p.toUpperCase().indexOf(str.toUpperCase()) === 0 && p !== str
-        );
+        let matchingPlayers = whisperablePlayers.filter((target) => {
+          const p = target.toUpperCase();
+          const i = input.toUpperCase();
+          return p.indexOf(i) >= 0 && p !== i && !alreadyTargeted.includes(p);
+        });
         if (matchingPlayers.length > 0) {
           let listOfPlayers = "";
-          // show ghost text to autocomplete if there's just one match
-          ghostText(matchingPlayers);
+          // show ghost text to autocomplete if there's a match starting with the characters already typed
+          ghostText(input, matchingPlayers);
           // set up and display the menu of whisperable names
           for (let p in matchingPlayers) {
             if (isNaN(p)) continue;
             const name = matchingPlayers[p];
-            listOfPlayers += `<li class="context-item" data-name="${name}"><i class="fas fa-male fa-fw"></i>${name}</li>`;
+            let nameHtml = name;
+            let startIndex = name.toUpperCase().indexOf(input.toUpperCase());
+            if (input && startIndex > -1) {
+              nameHtml = name.substr(0, startIndex) + "<strong>" + name.substr(startIndex, input.length) + "</strong>" + name.substr(startIndex + input.length);
+            }
+            listOfPlayers += `<li class="context-item" data-name="${name}"><i class="fas fa-male fa-fw"></i>${nameHtml}</li>`;
           }
           $whisperMenu.find("ol").html(listOfPlayers);
           $("#whisper-menu").html($whisperMenu);
-
+  
           // set up click-outside listener to close menu
           $(window).on("click.outsidewhispermenu", (e) => {
             var $target = $(e.target);
@@ -92,21 +98,63 @@ Hooks.on("ready", () => {
         closeWhisperMenu();
       }
     }
+  
+    function listFinishHandler(e) {
+      if (e.which == 221) {
+        // `]`
+        let val = $("#chat-message").val();
+        if (val.match(listOfNamesRegex)) {
+          if (typeof e === "object") e.preventDefault();
+          val = val.trim();
+          const newval = val.substring(0, val.length - 1); // remove `,` from the end
+          $("#chat-message").val(newval + "] ");
+          closeWhisperMenu();
+        }
+      }
+    }
+  
+    function menuItemClickHandler(e) {
+      e.stopPropagation();
+      var autocompleteText = autocomplete($(this).text());
+      $("#chat-message").val(autocompleteText);
+      $("#chat-message").focus();
+      closeWhisperMenu();
+      if ($("#chat-message").val().indexOf("[") > -1) {
+        handleChatInput();
+      }
+    }
     
-    function ghostText(matches) {
-      // show ghost text to autocomplete if there's just one match
-      if (matches.length === 1) {
-        var autocompleteText = autocomplete(matches[0]);
+    function getAlreadyTargeted(names) {
+      let arr = [];
+      if (typeof names !== "undefined") {
+        // remove all spaces, capitalize, and split by commas, then remove the last blank item
+        arr = names.replace(/\s/g, "").toUpperCase().split(",");
+        arr.pop();
+      }
+      // console.log(arr);
+      return arr;
+    }
+  
+    function ghostText(input, matches) {
+      // show ghost text to autocomplete if there's a match starting with the characters already typed
+      let filteredMatches = matches.filter((target) => {
+        const p = target.toUpperCase();
+        const i = input.toUpperCase();
+        return p.indexOf(i) === 0 && p !== i;
+      });
+      if (filteredMatches.length === 1) {
+        var autocompleteText = autocomplete(filteredMatches[0]);
         $(".chatghosttextarea").val(autocompleteText);
-        $(".chatghosttextarea").addClass('show');
-        $("#chat-message").on('keydown.ghosttab', e => {
-          if (e.which == 9) { // tab
+        $(".chatghosttextarea").addClass("show");
+        $("#chat-message").on("keydown.ghosttab", (e) => {
+          if (e.which == 9) {
+            // tab
             e.preventDefault();
             $("#chat-message").val($(".chatghosttextarea").val());
             resetGhostText();
             $("#chat-message").focus();
             closeWhisperMenu();
-            if ($("#chat-message").val().indexOf('[') > -1) {
+            if ($("#chat-message").val().indexOf("[") > -1) {
               handleChatInput();
             }
           }
@@ -115,31 +163,45 @@ Hooks.on("ready", () => {
         resetGhostText();
       }
     }
-    
+  
     function autocomplete(match) {
       if (!match) return;
-      var arr = $("#chat-message").val().split(whisperPattern);
-      var startingSyntax = arr[1];
-      var firstNameInArray = arr[2] || '';
-      var partialName = arr[arr.length - 1];
-      var restOfTheName = match.substr(partialName.length);
-      var charactersAfter = startingSyntax.indexOf('[') > -1 ? ', ' : ' ';
-      return startingSyntax + firstNameInArray + partialName + restOfTheName + charactersAfter;
+      const arr = $("#chat-message").val().split(whisperPattern);
+      let startingSyntax = arr[whisperSyntaxIndex];
+      const targetsAlreadyInArray = arr[targetsInArrayIndex] || "";
+      const typedCharacters = arr[arr.length - 1];
+      let nameToAdd = '';
+      if (match.toUpperCase().indexOf(typedCharacters.toUpperCase()) === 0) {
+        var restOfTheName = match.substr(typedCharacters.length);
+        nameToAdd = typedCharacters + restOfTheName;
+      } else {
+        nameToAdd = match;
+      }
+      const isArrayOfTargets = startingSyntax.indexOf("[") > -1;
+      // add a `, ` if this is an array of targets
+      var charactersAfter = isArrayOfTargets ? ", " : " ";
+      // interesting quirk: if the whisper target has a space in it, the @ syntax only works if the target name is wrapped in [].
+      if (nameToAdd.indexOf(" ") > -1 && !isArrayOfTargets) {
+        startingSyntax += "[";
+        charactersAfter = "] "; // go ahead and close the array now, since the user probably only intended one target.
+      }
+      return (
+        startingSyntax +
+        targetsAlreadyInArray +
+        nameToAdd +
+        charactersAfter
+      );
     }
-
+  
     function closeWhisperMenu() {
       $("#whisper-menu").empty();
       $(window).off("click.outsidewhispermenu");
       resetGhostText();
     }
-    
+  
     function resetGhostText() {
       $("#chat-message").off("keydown.ghosttab");
-      $(".chatghosttextarea").val("").removeClass('show');
+      $(".chatghosttextarea").val("").removeClass("show");
     }
-  })();
-
-});
-
-Hooks.on('renderSidebarTab', (app, html) => {
-});
+  
+  });
